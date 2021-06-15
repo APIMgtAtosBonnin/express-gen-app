f732373c7690c064a2e6f139bc07f3695896a25a
APIMgtAtosBonnin <pierre.bonnin@atos.net>
2021-06-15 10:18:38 +0100

    add link to routes

--- express-gen-app/routes/api_v3.js
+++ express-gen-app/routes/api_v3.js
@@ -4,10 +4,11 @@ var apiv3 = express.Router();
 
 //Routes
 apiv3.get('/', async function(req, res) {
-  const db = req.app.locals.db;
-  db.listCollections().toArray()
-  .then(response => res.status(200).json(response))
-  .catch(error => console.error(error));
+    const db = req.app.locals.db;
+    const collectionList = await db.listCollections().toArray();
+    var response = "Hello from APIv3 root route. <br>APIv3 uses a MongoDB connector to return available database collections. <br><br> Available collections: <br>";
+    collectionList.forEach(col => response += `- ${col.name}` + "<br>");
+    res.send(response);
 });
 
 apiv3.get('/clients', async function(req, res) {
--- express-gen-app/test.js
+++ express-gen-app/test.js
@@ -1,25 +1,37 @@
-const dbconfig = require('./secrets.json');
+require('dotenv').config();
 const { MongoClient } = require("mongodb");
 
+//Create a new MongoClient
+const client = new MongoClient(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
 
-// Create a new MongoClient
-// const client = new MongoClient(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-//
-// async function run() {
-//   try {
-//     // Connect the client to the server
-//     await client.connect();
-//
-//     // Establish and verify connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Connected successfully to server");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+    // Establish and verify connection
+    await client.db("projects_v1").command({ ping: 1 });
+    console.log("Connected successfully to server");
+
+    //Retrieve collections
+    await  listCollections(client);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+async function listCollections(client){
+  const db = client.db("projects_v1");
+  collectionList = await db.listCollections().toArray();
+  var i;
+  for (i = 0; i < collectionList.length; i++) {
+      console.log(collectionList[i].name);
+  }
+  // console.log("Available collections:");
+  // collectionList.name.forEach(col => console.log(` - ${col.name}`));
+};
